607921b871a443e0cea5b09a72fe684b53a845db
MinsangPark115 <pagemu@kaist.ac.kr>
2024-09-20 18:23:26 +0900

    initial hpu ver

--- classifier_lib.py
+++ classifier_lib.py
@@ -80,6 +80,7 @@ def get_grad_log_ratio(discriminator, vpsde, unnormalized_input, std_wve_t, img_
             tau = vpsde.compute_t_cos_from_t_lin(tau)
         tau = torch.ones(input.shape[0], device=tau.device) * tau
         log_ratio = get_log_ratio(discriminator, x_, tau, class_labels)
+        print(log_ratio)
         return log_ratio
     with torch.enable_grad():
         x_ = input.float().clone().detach().requires_grad_()
@@ -100,9 +101,6 @@ def get_log_ratio(discriminator, input, time, class_labels):
     if discriminator == None:
         return torch.zeros(input.shape[0], device=input.device)
     else:
-        print(input.device)
-        print(type(time))
-        print(type(class_labels))
         log_ratio = discriminator(input, timesteps=time, condition=class_labels)
         # prediction = torch.clip(logits, 1e-5, 1. - 1e-5)
         # log_ratio = torch.log(prediction / (1. - prediction))
