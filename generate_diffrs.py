8ca0dfbaa6dd4c92738805fc29126827ca60af05
MinsangPark115 <pagemu@kaist.ac.kr>
2024-09-20 21:47:15 +0900

    long del

--- generate_diffrs.py
+++ generate_diffrs.py
@@ -287,7 +287,8 @@ def diffrs_sampler(
                 x_fin[total_samples % batch_size:total_samples % batch_size + bool_fin.sum()] = x_next[bool_fin]
                 total_samples += bool_fin.sum()
             x_next[bool_fin] = torch.randn_like(x_next[bool_fin]).to(torch.float64) * t_steps[0]
-            print(lst_idx[bool_fin].device)
+            print(bool_fin.device)
+            print(lst_idx)
             lst_idx[bool_fin] = torch.zeros_like(lst_idx[bool_fin]).long()
             log_ratio_prev[bool_fin] = torch.zeros_like(log_ratio_prev[bool_fin])
 
