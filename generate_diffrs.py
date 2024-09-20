c70e1b14ce8efa4715e95bcd632c4e1d61711541
MinsangPark115 <pagemu@kaist.ac.kr>
2024-09-20 17:17:50 +0900

    initial hpu ver

--- generate_diffrs.py
+++ generate_diffrs.py
@@ -45,7 +45,12 @@ def diffrs_sampler(
         x_cur = x_next
 
         bool_zero = lst_idx == 0
-        print(x_cur[bool_zero].device, t_steps[lst_idx][bool_zero].device, net.img_resolution.device, time_min.device, time_max.device, labels.device)
+        print(x_cur[bool_zero].device)
+        print(t_steps[lst_idx][bool_zero].device)
+        print(net.img_resolution.device)
+        print(time_min.device)
+        print(time_max.device)
+        print(labels.device)
         if warmup:
             if bool_zero.sum() != 0:
                 log_ratio_prev[bool_zero] = classifier_lib.get_grad_log_ratio(discriminator, vpsde, x_cur[bool_zero], t_steps[lst_idx][bool_zero], net.img_resolution, time_min, time_max, labels, log_only=True).detach().cpu()
